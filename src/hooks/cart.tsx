3acd476b6ea8e7fd90199628d3b981ce7dddda96
Felizardo <devfelizardo@gmail.com>
2021-01-03 00:18:12 -0300

    creating function to increment itens on the cart page

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -64,9 +64,18 @@ const CartProvider: React.FC = ({ children }) => {
     [products],
   );
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      setProducts(
+        products.map(product =>
+          product.id === id
+            ? { ...product, quantity: product.quantity + 1 }
+            : product,
+        ),
+      );
+    },
+    [products],
+  );
 
   const decrement = useCallback(async id => {
     // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
